0946e6a8f1b870bed949664f0cfafee28c8157af
yangjh05 <yangjh05@naver.com>
2024-06-29 17:15:24 +0900

    today book fin?

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TodaysBookPage extends StatefulWidget {
   const TodaysBookPage({super.key});
@@ -11,28 +13,24 @@ class TodaysBookPage extends StatefulWidget {
 class _TodaysBookPageState extends State<TodaysBookPage> {
   int _currentBookIndex = 0;
   Timer? _timer;
-  final List<String> _bookImages = [
-    "assets/image/book1_1.png",
-    "assets/image/book1_2.png",
-    "assets/image/book2_1.png",
-    "assets/image/book2_2.png",
-    "assets/image/book3_1.png",
-    "assets/image/book3_2.png",
-    "assets/image/book4_1.png",
-    "assets/image/book5_1.png",
-    "assets/image/book5_2.png",
-    "assets/image/book6_1.png",
-    "assets/image/book6_2.png",
-    "assets/image/book7_1.png",
-    "assets/image/book7_2.png",
-    "assets/image/book8_1.png",
-    "assets/image/book8_2.png",
-    "assets/image/book9_1.png",
-    "assets/image/book9_2.png",
-    // 추가 이미지 파일 경로
-  ];
+  List<String> _bookImages = [];
+
+  Future<void> loadData() async {
+    final String response = await rootBundle.loadString('assets/books.json');
+    final List<dynamic> data = json.decode(response);
+    setState(() {
+      _bookImages = data.map((item) => item['image'] as String).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   void _startSlideshow() {
+    if (_timer != null) return;
     _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
       setState(() {
         _currentBookIndex = (_currentBookIndex + 1) % _bookImages.length;
@@ -42,6 +40,7 @@ class _TodaysBookPageState extends State<TodaysBookPage> {
 
   void _stopSlideshow() {
     _timer?.cancel();
+    _timer = null;
   }
 
   @override
@@ -53,45 +52,46 @@ class _TodaysBookPageState extends State<TodaysBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 20),
-          Text(
-            "Today's Book",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 20),
-          Expanded(
-            child: Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                height: 400,
-                child: Image.asset(
-                  _bookImages[_currentBookIndex],
-                  fit: BoxFit.fill,
-                ),
+      body: _bookImages.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Spacer(),
+                  Text(
+                    "Today's Book",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: 400,
+                    child: Image.asset(
+                      _bookImages[_currentBookIndex],
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _startSlideshow,
+                        child: Text('Start'),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: _stopSlideshow,
+                        child: Text('Stop'),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                ],
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: _startSlideshow,
-                child: Text('Start'),
-              ),
-              SizedBox(width: 20),
-              ElevatedButton(
-                onPressed: _stopSlideshow,
-                child: Text('Stop'),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-        ],
-      ),
     );
   }
 }
