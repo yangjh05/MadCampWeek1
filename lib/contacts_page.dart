6c5ea5d4fda00765f4b9090d00865f5ec70fb56f
daisythepotato <zjarhk22@sookmyung.ac.kr>
2024-06-29 17:05:55 +0900

    almost

--- lib/contacts_page.dart
+++ lib/contacts_page.dart
@@ -12,19 +12,49 @@ class ContactsPage extends StatefulWidget {
 
 class _ContactsPageState extends State<ContactsPage> {
   List<Contact> contacts = [];
+  List<Contact> filteredCon = [];
+  List<Contact> imageUrls = [];
+
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     loadContacts();
+    _searchController.addListener(filterItems);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void filterItems() {
+    String searchText = _searchController.text;
+    bool isEnglish = RegExp(r'[a-zA-Z]').hasMatch(searchText);
+    List<Contact> filtered = imageUrls.where((item) {
+      final itemLower = item.name.toLowerCase();
+      final matchesSearch = isEnglish
+          ? itemLower.contains(searchText.toLowerCase())
+          : item.name.contains(searchText);
+      return matchesSearch;
+    }).toList();
+
+    setState(() {
+      filteredCon = filtered;
+    });
   }
 
   Future<void> loadContacts() async {
     final String response = await rootBundle.loadString('assets/contacts.json');
     final data = json.decode(response);
     setState(() {
-      contacts = List<Contact>.from(data.map((item) => Contact.fromJson(item)));
-      contacts.sort((a, b) => a.name.compareTo(b.name));
+      imageUrls =
+          List<Contact>.from(data.map((item) => Contact.fromJson(item)));
+      imageUrls.sort((a, b) => a.name.compareTo(b.name));
+      contacts = imageUrls;
+      filteredCon = contacts;
     });
   }
 
@@ -33,36 +63,61 @@ class _ContactsPageState extends State<ContactsPage> {
     return MaterialApp(
         title: 'ListView',
         home: Scaffold(
-          appBar: AppBar(title: const Text('Contacts')),
-          body: contacts.isEmpty
-              ? Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  itemCount: contacts.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                        title: Text(contacts[index].name),
-                        subtitle: Text(contacts[index].book),
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey[400],
-                          backgroundImage: AssetImage(contacts[index].image),
+            appBar: AppBar(title: const Text('Contacts')),
+            body: contacts.isEmpty
+                ? Center(child: CircularProgressIndicator())
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _searchController,
+                          decoration: InputDecoration(
+                            labelText: 'Search',
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                          ),
                         ),
-                        trailing: const Icon(Icons.keyboard_arrow_right),
-                        onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SecondContacts(
-                                        number: contacts[index].number,
-                                        name: contacts[index].name,
-                                        book: contacts[index].book,
-                                        image: contacts[index].image,
-                                        birth: contacts[index].birth,
-                                        nation: contacts[index].nation,
-                                        education: contacts[index].education,
-                                      )),
-                            ));
-                  },
-                ),
-        ));
+                        SizedBox(height: 16),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: filteredCon.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(filteredCon[index].name),
+                                subtitle: Text(filteredCon[index].book),
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.grey[400],
+                                  backgroundImage:
+                                      AssetImage(filteredCon[index].image),
+                                ),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SecondContacts(
+                                            number: filteredCon[index].number,
+                                            name: filteredCon[index].name,
+                                            book: filteredCon[index].book,
+                                            image: filteredCon[index].image,
+                                            birth: filteredCon[index].birth,
+                                            nation: filteredCon[index].nation,
+                                            education:
+                                                filteredCon[index].education,
+                                          )),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )));
   }
 }
 
